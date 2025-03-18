42f09721488455e15f05e102ef894a5374b0d2fb
ManoelViktorovManev <mani.manev@gmail.com>
2025-03-18 16:13:30 +0200

    Adding a config/rdoutes.php file that we can use to create Routes without using Attributes

---
+++ config/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Config;
+
+use App\Core\Router;
+use App\Controller\NewPhpRouteImp;
+
+return function (Router $routes): void {
+    $routes->add('izwajdane', '/minus/{param1}/{param2}')
+        ->controller(NewPhpRouteImp::class, 'minusNa2Chisla');
+
+    $routes->add('obratno', '/reverse/{param}')
+        ->controller(NewPhpRouteImp::class, 'revers');
+};
