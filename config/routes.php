3195a7e06814d166bce711f179b0182ad2b88ee3
ManoelViktorovManev <mani.manev@gmail.com>
2025-03-21 22:06:20 +0200

    Updating the izwajdane to izwajdane12 after fixing the bug

--- config/routes.php
+++ config/routes.php
@@ -6,7 +6,7 @@ use App\Core\Router;
 use App\Controller\NewPhpRouteImp;
 
 return function (Router $routes): void {
-    $routes->add('izwajdane', '/minus/{param1}/{param2}')
+    $routes->add('izwajdane12', '/minus/{param1}/{param2}')
         ->controller(NewPhpRouteImp::class, 'minusNa2Chisla');
 
     $routes->add('obratno', '/reverse/{param}')
