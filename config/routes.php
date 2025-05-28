8d87d105cbc3a1becb2c68a2d5e111cbbf92b0a2
ManoelViktorovManev <mani.manev@gmail.com>
2025-05-28 20:07:58 +0300

    Updating the Config files

--- config/routes.php
+++ config/routes.php
@@ -3,12 +3,7 @@
 namespace App\Config;
 
 use App\Core\Router;
-use App\Controller\NewPhpRouteImp;
 
 return function (Router $routes): void {
-    $routes->add('izwajdane12', '/minus/{param1}/{param2}')
-        ->controller(NewPhpRouteImp::class, 'minusNa2Chisla');
-
-    $routes->add('obratno', '/reverse/{param}')
-        ->controller(NewPhpRouteImp::class, 'revers');
+    # $routes->add($name,$path)->controller($CLASS,$method)
 };
